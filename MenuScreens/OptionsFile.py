0292f61545c59ace5e8085d473b15914ea48455f
Kar1mAhmed <karim77645@gmail.com>
2023-05-06 10:55:00 +0300

    Added game settings to options

--- MenuScreens/OptionsFile.py
+++ MenuScreens/OptionsFile.py
@@ -3,11 +3,13 @@ import main
 import sys
 from Helpers.button import Button
 
+import settings
+
 
 def options(screen_width, screen_height):
     buttons_posX = screen_width / 2
     y_start = screen_height / 2 - 100
-    buttons_posY = [y_start, y_start + 100, y_start + 300]
+    buttons_posY = [y_start - 300 ,y_start - 200, y_start - 100, y_start , y_start + 100 ,y_start + 300, y_start + 450]
     
     Music_On = True
     Music_label = "Mute Music"
@@ -18,16 +20,36 @@ def options(screen_width, screen_height):
 
         main.SCREEN.blit(main.BG, (0, 0))
 
-        SOUND_BUTTON_MUTE = Button(image=None, pos=(buttons_posX, buttons_posY[0]),
+        info = Button(image=None, pos=(buttons_posX, 50),
+                            text_input="Left Click to increase - Right Click to decrease",
+                            font=main.get_font(20), base_color="White", hovering_color="White")
+
+        Maze_rows = Button(image=None, pos=(buttons_posX, buttons_posY[0]),
+                            text_input=f"Number of Maze Columns\t {settings.Number_of_rows}",
+                            font=main.get_font(40), base_color="White", hovering_color="#7400b8")
+        
+        Maze_Cols = Button(image=None, pos=(buttons_posX, buttons_posY[1]),
+                            text_input=f"Number of Maze Columns\t {settings.Number_of_cols}",
+                            font=main.get_font(40), base_color="White", hovering_color="#6930c3")
+        
+        Kids = Button(image=None, pos=(buttons_posX, buttons_posY[2]),
+                        text_input=f"Number of Kids\t {settings.Num_of_kids}",
+                        font=main.get_font(40), base_color="White", hovering_color="#5e60ce")
+        
+        Time = Button(image=None, pos=(buttons_posX, buttons_posY[3]),
+                        text_input=f"Bomb Time :\t {settings.Level_time}",
+                        font=main.get_font(40), base_color="White", hovering_color="#5390d9")
+
+        SOUND_BUTTON_MUTE = Button(image=None, pos=(buttons_posX, buttons_posY[4]),
                                    text_input=Music_label, font=main.get_font(60), base_color="White",
                                    hovering_color="#4cc9f0")
 
 
-        OPTIONS_BACK = Button(image=None, pos=(buttons_posX, buttons_posY[2]),
+        OPTIONS_BACK = Button(image=None, pos=(buttons_posX, buttons_posY[5]),
                               text_input="BACK", font=main.get_font(75), base_color="White",
                               hovering_color="#ff006e")
 
-        for button in [OPTIONS_BACK, SOUND_BUTTON_MUTE]:
+        for button in [OPTIONS_BACK, SOUND_BUTTON_MUTE, Kids, Maze_rows, Maze_Cols, Time,]:
             button.changeColor(OPTIONS_MOUSE_POS)
             button.update(main.SCREEN)
 
@@ -47,6 +69,42 @@ def options(screen_width, screen_height):
                         main.mixer.music.play()
                         Music_label = "Mute Music"
                         Music_On = True
+                        
+                if Time.checkForInput(OPTIONS_MOUSE_POS):
+                    if event.button == 1:
+                        if settings.Level_time == "default":
+                            settings.Level_time = 10
+                        else:
+                            settings.Level_time += 5
+                    elif event.button == 3:
+                        if settings.Level_time != "default":
+                            settings.Level_time -=5
+                        if settings.Level_time == 'default' or settings.Level_time <= 0:
+                            settings.Level_time = "default"
+                            
+                if Kids.checkForInput(OPTIONS_MOUSE_POS):
+                    if event.button == 1:
+                        if settings.Num_of_kids < 100:
+                            settings.Num_of_kids +=1
+                    elif event.button == 3:
+                        if settings.Num_of_kids > 1:
+                            settings.Num_of_kids -= 1
+                            
+                if Maze_Cols.checkForInput(OPTIONS_MOUSE_POS):
+                    if event.button == 1:
+                        if settings.Number_of_cols < 100:
+                            settings.Number_of_cols += 5
+                    elif event.button == 3:
+                        if settings.Number_of_cols > 5:
+                            settings.Number_of_cols -= 5
+
+                if Maze_rows.checkForInput(OPTIONS_MOUSE_POS):
+                    if event.button == 1:
+                        if settings.Number_of_rows < 100:
+                            settings.Number_of_rows += 5
+                    elif event.button == 3:
+                        if settings.Number_of_rows > 5:
+                            settings.Number_of_rows -= 5
 
 
         pygame.display.update()
--- MenuScreens/StartGame.py
+++ MenuScreens/StartGame.py
@@ -4,7 +4,6 @@ from Helpers.button import Button
 import sys
 from Game.game_play import Game
 
-import settings
 
 game = Game()
 
@@ -12,41 +11,22 @@ def ChooseYourAlgo(screen_width, screen_height):
     
     buttons_posX = screen_width / 2
     y_start = screen_height / 2 - 100
-    buttons_posY = [y_start - 200, y_start - 100, y_start , y_start + 100 ,y_start + 300, y_start + 450]
+    buttons_posY = [y_start, y_start + 200]
     while True:
         main.SCREEN.blit(main.BG, (0, 0))
 
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-        info = Button(image=None, pos=(buttons_posX, 50),
-                            text_input="Left Click to increase - Right Click to decrease",
-                            font=main.get_font(20), base_color="White", hovering_color="White")
-
-        Maze_rows = Button(image=None, pos=(buttons_posX, buttons_posY[0]),
-                            text_input=f"Number of Maze Columns\t {settings.Number_of_rows}",
-                            font=main.get_font(40), base_color="White", hovering_color="#7400b8")
         
-        Maze_Cols = Button(image=None, pos=(buttons_posX, buttons_posY[1]),
-                            text_input=f"Number of Maze Columns\t {settings.Number_of_cols}",
-                            font=main.get_font(40), base_color="White", hovering_color="#6930c3")
-        
-        Kids = Button(image=None, pos=(buttons_posX, buttons_posY[2]),
-                        text_input=f"Number of Kids\t {settings.Num_of_kids}",
-                        font=main.get_font(40), base_color="White", hovering_color="#5e60ce")
-        
-        Time = Button(image=None, pos=(buttons_posX, buttons_posY[3]),
-                        text_input=f"Bomb Time :\t {settings.Level_time}",
-                        font=main.get_font(40), base_color="White", hovering_color="#5390d9")
-        
-        START_NEW_GAME = Button(image=None, pos=(buttons_posX, buttons_posY[4]),
-                                text_input="Enter The Game", font=main.get_font(60), base_color="White",
+        START_NEW_GAME = Button(image=None, pos=(buttons_posX, buttons_posY[0]),
+                                text_input="Enter The Game", font=main.get_font(80), base_color="White",
                                 hovering_color="#758bfd")
         
-        Menu_BACK = Button(image=None, pos=(buttons_posX, buttons_posY[5]),text_input="BACK",
-                            font=main.get_font(60), base_color="White", hovering_color="#ff006e")
+        Menu_BACK = Button(image=None, pos=(buttons_posX, buttons_posY[1]),text_input="BACK",
+                            font=main.get_font(80), base_color="White", hovering_color="#ff006e")
 
 
-        for button in [Kids, Maze_rows, Maze_Cols, Time, START_NEW_GAME, Menu_BACK, info]:
+        for button in [START_NEW_GAME, Menu_BACK]:
             button.changeColor(PLAY_MOUSE_POS)
             button.update(main.SCREEN)
 
@@ -63,41 +43,6 @@ def ChooseYourAlgo(screen_width, screen_height):
                 if Menu_BACK.checkForInput(PLAY_MOUSE_POS):
                     main.main_menu()
                     
-                if Time.checkForInput(PLAY_MOUSE_POS):
-                    if event.button == 1:
-                        if settings.Level_time == "default":
-                            settings.Level_time = 10
-                        else:
-                            settings.Level_time += 5
-                    elif event.button == 3:
-                        if settings.Level_time != "default":
-                            settings.Level_time -=5
-                        if settings.Level_time == 'default' or settings.Level_time <= 0:
-                            settings.Level_time = "default"
-                            
-                if Kids.checkForInput(PLAY_MOUSE_POS):
-                    if event.button == 1:
-                        if settings.Num_of_kids < 100:
-                            settings.Num_of_kids +=1
-                    elif event.button == 3:
-                        if settings.Num_of_kids > 1:
-                            settings.Num_of_kids -= 1
-                            
-                if Maze_Cols.checkForInput(PLAY_MOUSE_POS):
-                    if event.button == 1:
-                        if settings.Number_of_cols < 100:
-                            settings.Number_of_cols += 5
-                    elif event.button == 3:
-                        if settings.Number_of_cols > 5:
-                            settings.Number_of_cols -= 5
-
-                if Maze_rows.checkForInput(PLAY_MOUSE_POS):
-                    if event.button == 1:
-                        if settings.Number_of_rows < 100:
-                            settings.Number_of_rows += 5
-                    elif event.button == 3:
-                        if settings.Number_of_rows > 5:
-                            settings.Number_of_rows -= 5
 
 
         pygame.display.update()
